ddef420e60c34d4083e5868ed6a87f6400235750
Franklin Kong <franklinkong981@gmail.com>
2024-01-03 14:28:46 -0800

    Added flash messages when user goes to wrong URL

--- app.py
+++ app.py
@@ -12,17 +12,21 @@ USER_RESPONSES = []
 @app.route('/')
 def show_main_page():
     if len(USER_RESPONSES) >= len(satisfaction_survey.questions):
+        flash("You've already completed the survey!")
         return redirect('/thanks')
     elif len(USER_RESPONSES) != 0:
+        flash("You've still got some questions to answer!")
         return redirect('/questions/' + str(len(USER_RESPONSES))) 
     return render_template("main.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
 
 @app.route('/questions/<q_number>')
 def show_question(q_number):
     q_number = int(q_number)
-    if q_number != len(USER_RESPONSES):
+    if q_number != len(USER_RESPONSES) and len(USER_RESPONSES) < len(satisfaction_survey.questions):
+        flash("You've tried to go to the wrong question! Here is the next one.")
         return redirect('/questions/' + str(len(USER_RESPONSES)))
     elif len(USER_RESPONSES) >= len(satisfaction_survey.questions):
+        flash("You've already completed the survey!")
         return redirect('/thanks')
     current_question = satisfaction_survey.questions[q_number]
     return render_template("question.html", question_number=q_number, question_text=current_question.question, choices = current_question.choices)
@@ -39,7 +43,9 @@ def add_response():
 @app.route('/thanks')
 def show_thanks():
     if len(USER_RESPONSES) == 0:
+        flash("You must complete the survey to reach the thank you page!")
         return redirect('/')
     elif len(USER_RESPONSES) < len(satisfaction_survey.questions):
+        flash("You still have some questions to complete!")
         return redirect('/questions/' + str(len(USER_RESPONSES)))
     return render_template("thanks.html", survey_name=satisfaction_survey.title)
