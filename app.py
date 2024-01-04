52cf067332aa3a7aab9e82686c1f002e9c74a5b2
Franklin Kong <franklinkong981@gmail.com>
2024-01-04 14:34:57 -0800

    Used Flask session to store responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Survey, satisfaction_survey
 
@@ -7,25 +7,34 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-USER_RESPONSES = []
-
 @app.route('/')
 def show_main_page():
-    if len(USER_RESPONSES) >= len(satisfaction_survey.questions):
-        flash("You've already completed the survey!")
-        return redirect('/thanks')
-    elif len(USER_RESPONSES) != 0:
-        flash("You've still got some questions to answer!")
-        return redirect('/questions/' + str(len(USER_RESPONSES))) 
+    user_responses = session.get('responses', None)
+    if user_responses is not None:
+        if len(user_responses) >= len(satisfaction_survey.questions):
+            flash("You've already completed the survey!")
+            return redirect('/thanks')
+        elif len(user_responses) != 0:
+            flash("You've still got some questions to answer!")
+            return redirect('/questions/' + str(len(user_responses))) 
     return render_template("main.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
+
+@app.route('/start-survey', methods=["POST"])
+def start_survey():
+    session['responses'] = []
+    return redirect('/questions/0')
 
 @app.route('/questions/<q_number>')
 def show_question(q_number):
+    if session.get('responses', None) is None:
+        flash("Please click the button to start the survey.")
+        return redirect('/')
+    
     q_number = int(q_number)
-    if q_number != len(USER_RESPONSES) and len(USER_RESPONSES) < len(satisfaction_survey.questions):
+    if q_number != len(session["responses"]) and len(session["responses"]) < len(satisfaction_survey.questions):
         flash("You've tried to go to the wrong question! Here is the next one.")
-        return redirect('/questions/' + str(len(USER_RESPONSES)))
-    elif len(USER_RESPONSES) >= len(satisfaction_survey.questions):
+        return redirect('/questions/' + str(len(session["responses"])))
+    elif len(session["responses"]) >= len(satisfaction_survey.questions):
         flash("You've already completed the survey!")
         return redirect('/thanks')
     current_question = satisfaction_survey.questions[q_number]
@@ -35,17 +44,20 @@ def show_question(q_number):
 def add_response():
     answer = request.form["answer"]
     # add answer to the list of user responses
-    USER_RESPONSES.append(answer)
-    if len(USER_RESPONSES) >= len(satisfaction_survey.questions):
+    user_responses = session["responses"]
+    user_responses.append(answer)
+    session["responses"] = user_responses
+    
+    if len(session["responses"]) >= len(satisfaction_survey.questions):
         return redirect('/thanks')
-    return redirect('/questions/' + str(len(USER_RESPONSES)))
+    return redirect('/questions/' + str(len(session["responses"])))
 
 @app.route('/thanks')
 def show_thanks():
-    if len(USER_RESPONSES) == 0:
-        flash("You must complete the survey to reach the thank you page!")
+    if session.get('responses', None) is None:
+        flash("Please click the button to start the survey.")
         return redirect('/')
-    elif len(USER_RESPONSES) < len(satisfaction_survey.questions):
+    elif len(session['responses']) < len(satisfaction_survey.questions):
         flash("You still have some questions to complete!")
-        return redirect('/questions/' + str(len(USER_RESPONSES)))
+        return redirect('/questions/' + str(len(session['responses'])))
     return render_template("thanks.html", survey_name=satisfaction_survey.title)
